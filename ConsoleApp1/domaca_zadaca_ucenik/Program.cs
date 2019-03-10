23b371661569f21b7ca1ad2a9e9c1c298c49a120
herbishealing <slaven.bestvina@gmail.com>
2019-03-10 18:51:34 +0100

    kostur

---
+++ ConsoleApp1/domaca_zadaca_ucenik/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domaca_zadaca_ucenik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Ucenik U1 = new Ucenik(Andrija, Andrasek, 1111111111111, r1);
+            Ucenik U2 = new Ucenik(Boris, Barisic, 2222222222222, r2);
+            Ucenik U3 = new Ucenik(Drago, Dragic, 3333333333333, r3);
+
+            List<Ucenik>listaUcenika = new List<Ucenik>();
+            listaUcenika.Add(U1);
+            listaUcenika.Add(U2);
+            listaUcenika.Add(U3);
+
+        }
+    }
+}
---
+++ ConsoleApp1/domaca_zadaca_ucenik/Ucenik.cs
@@ -0,0 +1,78 @@
+﻿namespace domaca_zadaca_ucenik
+{
+     class Ucenik
+    {
+        #region svojstva
+
+        private string ime;
+        public string Ime
+        {
+            get { return ime; }
+            set { ime = value; }
+        }
+
+        private string prezime;
+        public string Prezime
+        {
+            get { return prezime; }
+            set { prezime = value; }
+        }
+
+        private string oib;
+        public string OIB
+        {
+            get { return oib; }
+            set { oib = value; }
+        }
+
+
+        private string racun;
+        public string Racun
+        {
+            get { return racun; }
+            set { racun = value; }
+        }
+
+        private string telefon;
+        private string Telefon
+        {
+            get { return telefon; }
+            set { telefon = value; }
+        }
+
+        #endregion
+        #region metode
+
+        public override string ToString()
+        {
+            return "Pozdrav" + "Ucenik" + ime + prezime + racun;
+        }
+
+
+        #endregion
+        #region konstruktori
+
+        public Ucenik() { }
+
+        public Ucenik(string ime, string prezime)
+        {
+            this.Ime = ime;
+            this.Prezime = prezime;
+        }
+
+        public Ucenik(string ime, string prezime, string oib)
+          : this(ime, prezime)
+        {
+            this.OIB = oib;
+        }
+
+        public Ucenik (string ime, string prezime, string oib, string racun)
+            :this(ime, prezime,oib)
+        {
+            this.Racun = racun;
+        }
+
+
+        #endregion
+    }
+}
