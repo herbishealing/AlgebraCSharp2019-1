84a3edeae3f1d57be733f3b25d2d24008d842351
sunflow3r <slaven.bestvina@gmail.com>
2019-03-13 18:08:50 +0100

    13.3 eventi

---
+++ ConsoleApp1/8.3.1_nasljedivanje/Osoba.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _8._3._1_nasljedivanje
+{
+    internal class Osoba
+    {
+        public DateTime datumNastanka;
+
+        public Osoba()
+        {
+            this.datumNastanka = DateTime.Now;
+            Console.WriteLine("pozvana metoda Osoba:Osoba()");
+
+        }
+
+    }
+}
