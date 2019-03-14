f5babd8725c3356783a7ea38b9d807a76ca27eb6
sunflow3r <slaven.bestvina@gmail.com>
2019-03-14 17:10:23 +0100

    primjeri

---
+++ ConsoleApp1/10.1.1_datoteke/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._1._1_datoteke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("molim upisite neki tekst");
+            string upis = Console.ReadLine();
+
+            //kreiranje handle-a
+            FileStream fs1 = new FileStream("Proba.txt",FileMode.Create);
+            //zapisivanje u datoteku
+            StreamWriter sw1 = new StreamWriter(fs1);
+            sw1.WriteLine(upis);
+            sw1.Flush();
+            sw1.Close();
+            fs1.Close();
+
+            //citanje iz datoteke Proba.txt
+            FileStream fs2 = new FileStream("Proba.txt", FileMode.Open);
+            StreamReader sr1 = new StreamReader(fs2);
+            string procitano = sr1.ReadToEnd();
+            Console.WriteLine("ispis iz datoteke Proba.txt");
+            Console.WriteLine(procitano);
+
+
+            Console.ReadKey();
+        }
+    }
+}
