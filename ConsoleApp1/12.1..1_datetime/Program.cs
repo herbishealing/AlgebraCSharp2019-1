98c3d9f83cc89673ff9abfa917de8d7d02868043
sunflow3r <slaven.bestvina@gmail.com>
2019-03-15 17:34:30 +0100

    prvi dio

---
+++ ConsoleApp1/12.1..1_datetime/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._1._._1_datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime datum1 = new DateTime(2019, 2, 28);
+            DateTime datum2 = new DateTime(2019, 3, 28);
+            DateTime datum3 = new DateTime(2019, 2, 28);
+
+            if (datum1 < datum2)
+            {
+                Console.WriteLine(datum1.ToString()+" je manji od datuma "+datum2.ToString());
+            }
+            else
+            {
+                Console.WriteLine(datum1.ToString() + " je veci od datuma " + datum2.ToString());
+            }
+            Console.WriteLine("trenutno je:"+DateTime.Now);
+            Console.WriteLine("trenutno je:" + DateTime.Today);
+            //(uvijet)?true:false
+            Console.WriteLine((DateTime.IsLeapYear(2019))? "ovo je prijestupna godina?":"ovo nije prijestupna godina");
+
+            Console.WriteLine(datum1 +"datum + 7 dana:"+datum1.AddDays(7));
+            Console.WriteLine("danas je:" + DateTime.Today.DayOfWeek);
+
+            Console.ReadLine();
+        }
+    }
+}
