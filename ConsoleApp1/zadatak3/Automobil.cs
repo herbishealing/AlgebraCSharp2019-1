e483a2ff8853c68e10b499ebb0d2f0b71bd6796b
sunflow3r <slaven.bestvina@gmail.com>
2019-03-15 19:03:20 +0100

    ispit

---
+++ ConsoleApp1/zadatak3/Automobil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak3
+{
+    class Automobil
+    {
+        string Naziv;
+        int GodinaProiuvodnje;
+        double OsnovnaCijena;
+
+        public string Naziv1 { get => Naziv; set => Naziv = value; }
+        public int GodinaProiuvodnje1 { get => GodinaProiuvodnje; set => GodinaProiuvodnje = value; }
+        public double OsnovnaCijena1 { get => OsnovnaCijena; set => OsnovnaCijena = value; }
+
+        public class Starost
+        {
+            DateTime datum = new DateTime();
+            
+        }
+    }
+}
---
+++ ConsoleApp1/zadatak_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("unesi broj: ");
+           int broj = int.Parse(Console.ReadLine());
+            int obrnuto = 0;
+
+            while (broj > 0)
+            {
+                int ostatak = broj % 10;
+                obrnuto = (obrnuto * 10) + ostatak;
+                broj = broj / 10;
+            }
+            Console.WriteLine("obrnuti broj je {0}!", obrnuto);
+            Console.ReadKey();
+        }
+    }
+}
